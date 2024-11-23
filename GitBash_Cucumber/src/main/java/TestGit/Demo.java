eafa6bc3ea6a267297c3f64208ac915b3f36b859
Amar <amarkadam7240@gmail.com>
2024-11-23 21:21:43 +0530

    modified

--- GitBash_Cucumber/src/main/java/TestGit/Demo.java
+++ GitBash_Cucumber/src/main/java/TestGit/Demo.java
@@ -3,8 +3,8 @@ package TestGit;
 public class Demo {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("World");
+		System.out.println("Hello-Amar");
+		System.out.println("World-HelloS");
 	}
 
 }
